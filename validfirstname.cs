a154e58d8273d1e8b2bd917dd03c1ae69de9157d
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:38:56 +0530

    first name with cap min 3 chars

---
+++ validfirstname.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class validfirstname
+    {
+        public static string regexrule = (@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public bool validatefirstname(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
