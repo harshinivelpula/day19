34cf3eee7f875aa3ee87b576f2aa7887de418953
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:43:04 +0530

    rule2 with atleast 1 upper char min 8 chars

---
+++ passwordrule2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class passwordrule2
+    {
+        public static string regexrule = ("^[A-Z]{1,8}[a-z0-9]{7,}$");
+        public bool validatePassWord2(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
