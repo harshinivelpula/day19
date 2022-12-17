f178b60f00aa6fb7d39bb5f32f9d0d1ba1d2ae82
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:45:11 +0530

    clear all sample in list

---
+++ emailsamples.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class emailsamples
+    {
+        public static string regexrule = "^[a-z]{3}([.+-@])?([a-z]+)?([0-9]+)?([@])?([a-z]+)?[.][a-z]{3}([,.])?([a-z]+)?$";
+        public bool validateallemails(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
