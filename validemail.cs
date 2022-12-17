f25d2a00053f0884c09039d6a13e321d56bac701
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:40:50 +0530

    email with 3 mandatory and 2 optional parts with @ and . position

---
+++ validemail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class validemail
+    {
+        public static string regexrule = ("^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}[.][A-Za-z]{2,}([.][A-Za-z]{2})?$");
+        public bool validateemail(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
