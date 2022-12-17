d1d95e488ac87eda51e403a7a6351476084236ed
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:43:56 +0530

    rule3 with atleast 1 number min 8 chars atleast 1 uppercase

---
+++ passwordrule3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class passwordrule3
+    {
+        public static string regexrule = "^[A-Za-z]{6,}[A-Z]{1}[0-9]{1,}$";
+        public bool validatePassWord3(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
