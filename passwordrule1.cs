4ba3b6a1b75849cde1142eaa95b082c6558b2df9
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:42:17 +0530

    rule1 with min 8 chars

---
+++ passwordrule1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class passwordrule1
+    {
+        public static string regexrule = "[A-Za-z0-9]{8,}";
+        public bool validatePassWord1(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
