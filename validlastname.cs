66d23093c5daab0a9b64c188a62581bfdeee6c31
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:39:48 +0530

    last name with cap min 3 chars

---
+++ validlastname.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class validlastname
+    {
+        public static string regexrule = (@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public bool validatelastname(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
