b4970d8b13ad39899564baccf01221c9d5347f5e
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 17:41:42 +0530

    number with countrycode followed by space and 10digit number

---
+++ mobileformat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User19
+{
+    public class mobileformat
+    {
+        public static string regexrule = ("[1-9][0-9][ ]?[6-9][0-9]{9}$");
+        public bool validatemobilenumber(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, regexrule);
+        }
+    }
+}
